463dce107722f3055d1bb80e2a40408cd649948e
Jose Albizures <albizures3601@gmail.com>
2015-06-23 18:24:31 -0600

    /
    /

--- Server_Deploy/controllers/reportes/reportes.controller.js
+++ Server_Deploy/controllers/reportes/reportes.controller.js
@@ -60,8 +60,8 @@ exports.generarFacturasPDF = function (req,res) {
 	console.log('wait...');
 	require("jsreport").render({
 		template: {
-			content : style+'<body><table class="table table-striped servicos-factura"> <thead> <th>Factura</th><th>Fecha</th><th>Nombre</th><th>Paciente</th><th>Direccion</th><th>Servicio</th><th>Precio</th><th>Subtotal</th> </thead> <tbody>{{for facturas}}<tr> <td>{{:serie}}-{{:numero}}</td><td>{{:~dateNow(fecha)}}</td><td>{{:nombre}}</td><td>{{:paciente}}</td><td>{{:direccion}}</td><td>{{:nombreServico}}</td><td>Q{{:~money(precio)}}</td><td>Q{{:~money(monto)}}</td></tr>{{/for}}</tbody></table></body>',
-			helpers : 'function dateNow(){var date=new Date(); return date.getDate() + "/" +(date.getMonth() + 1) + "/" + date.getFullYear();};function money(numero){return numero.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");}',
+			content : style+'<body><table class="table table-striped servicos-factura"> <thead> <th>Factura</th><th>Fecha</th><th>Nombre</th><th>Paciente</th><th>Direccion</th><th>Servicio</th><th>Precio</th><th>Subtotal</th> </thead> <tbody>{{for facturas}}<tr> <td>{{:serie}}-{{:numero}}</td><td>{{:~dateNow(fecha)}}</td><td>{{:nombre}}</td><td>{{:paciente}}</td><td>{{:direccion}}</td><td>{{:nombreServico}}</td><td>Q{{:~anulado(estado,monto)}}</td><td>Q{{:~anulado(estado,monto)}}</td></tr>{{/for}}</tbody></table></body>',
+			helpers : 'function dateNow(){var date=new Date(); return date.getDate() + "/" +(date.getMonth() + 1) + "/" + date.getFullYear();};function money(numero){return numero.toFixed(2).replace(/(\d)(?=(\d{3})+\.)/g, "$1,");};function anulado(estado,numero){ if(estado.toLowerCase() == "anulado"){return "Anulado"}else{return money(numero)}}',
 			engine: "jsrender",
 			phantom : {
 				footer : "<div style='text-align:center'>{#pageNum}/{#numPages}</div>",
